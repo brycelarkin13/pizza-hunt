5d3d27b5afa3070d38157db4e2cd6023c2655cac
bryce <blarkin13@gmail.com>
2022-04-22 19:15:55 -0500

    working crud

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -6,7 +6,7 @@ const {
     createPizza,
     updatePizza,
     deletePizza
-  } = require('../../controllers/pizza-controller');
+  } = require('../../controllers/pizza-controllers');
 
 // Set up GET all and POST at /api/pizzas
 router
